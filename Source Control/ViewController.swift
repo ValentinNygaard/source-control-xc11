ed5f2941bf2e49f490ae3b7241e097e4c51096e7
ValentinNygaard <vn@rvnc.dk>
2020-01-24 10:23:02 +0100

    Initial Commit

---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Valentin Nygaard on 24/01/2020.
+//  Copyright © 2020 Valentin Nygaard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
